21b6825b33f402869c9ee4a1e289c951b4062002
muccidavide <davidemucci0804@gmail.com>
2022-06-14 14:20:59 +0200

    initial commit~

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="cards_travel">
+            <h1>Prova</h1>
+        </div>
+    </div>
+</div>
+
+@endsection
+
